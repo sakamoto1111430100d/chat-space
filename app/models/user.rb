a69651973c199f196ee871083cc93bb41fb0a25e
Sakamoto Takaaki <st.sari66@gmail.com>
2020-05-26 20:59:21 +0900

    Create register edit function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, uniqueness: true
-
-  has_many :messages
-  has_many :users_groups
-  has_many :groups, through: :users_groups  
-
+  has_many :group_users
+  has_many :groups, through: :group_users  
 end
